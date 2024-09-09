7a8b26cbc9b7c545f63c505efd66a06304e40ce3
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2024-09-09 19:36:26 +0000

    branch update from codesandbox

--- app/template.jsx
+++ app/template.jsx
@@ -1,36 +1,49 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 //hooks
-import useScrollProgress from '@/hooks/useScrollProgress';
+import useScrollProgress from "@/hooks/useScrollProgress";
 
 //variants
 const variants = {
   hidden: {
-    opacity: 0
+    opacity: 0,
   },
   enter: {
-    opacity: 1
-  }
-}
+    opacity: 1,
+  },
+};
 
 const Template = ({ children }) => {
-  const complete = useScrollProgress();
-
+  const completion = useScrollProgress();
 
   return (
     <>
-      <motion.main variants={variants} initial="hidden" animate={complete ? "enter" : "hidden"}
-        transition={{ type: 'linear', delay: 0.2, duration: 0.4 }}>
+      <motion.main
+        variants={variants}
+        initial="hidden"
+        animate={completion ? "enter" : "hidden"}
+        transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
+      >
         {children}
       </motion.main>
       {/* completion bar */}
-      <span className="fixed" z50 big-primary-bg bottom-0 left-0 right-0 h-1>
-
-      </span>
+      <span
+        sytle={{ transform: `translateY(${completion - 100}%)` }}
+        className="fixed
+        z50
+        big-primary-bg
+        w-1
+        top-0
+        right-0
+        bottom-0
+        transition-all
+        duration-700"
+      ></span>
+      <div className="h-[4000px]"></div>
     </>
-  )
-}
+  );
+};
 
 export default Template;
