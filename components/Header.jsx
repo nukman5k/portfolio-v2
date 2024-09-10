03e8b9d2579f4ae0fd40ef2e89ba48c12a8ed707
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2024-09-10 15:24:40 +0000

    Header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,12 +2,13 @@
 // import React, { useEffect, useState } from "react";
 
 //components
-
+import Logo from "./Logo";
 import ThemeToggler from "./ThemeToggler";
 
 const Header = () => {
   return (
     <header>
+      <div className="container mx-auto"></div>
       <ThemeToggler />
     </header>
   );
--- components/Logo.jsx
+++ components/Logo.jsx
@@ -7,3 +7,4 @@ const Logo = () => {
 }
 
 export default Logo
+
