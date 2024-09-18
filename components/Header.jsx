8b58d2095b3db01b1ff02ba3272fc31d385ba4ad
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2024-09-18 20:12:20 +0000

    update 5 (complete project today)

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,19 +6,43 @@ import Logo from "./Logo";
 import MobileNav from "./MobileNav";
 import Nav from "./Nav";
 import ThemeToggler from "./ThemeToggler";
+import { usePathname } from "next/navigation";
+import { SheetHeader } from "./ui/sheet";
 
 const Header = () => {
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+  console.log(pathname);
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+
+    //remove event
+    return () => window.removeEventListener("scroll", scrollYPos);
+  }, []);
+
   return (
-    <header>
+    <header
+      className={`${
+        header
+          ? "py-4 bg-white shadow-lg dark:bg-accent"
+          : "py-6 dark:bg-transparent"
+      } sticky top-0 z-30 transition-all ${pathname === "/" && "bg-[#fef9f5]"}`}
+    >
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <Logo />
           <div className="flex items-center gap-x-6">
             {/* nav */}
-            <Nav containerStyles="hidden lg:flex xl:flex gap-x-8 items-center" linkStyles='relative hover:text-primary transition-all'
-              underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full' />
+            <Nav
+              containerStyles="hidden lg:flex xl:flex gap-x-8 items-center"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
             {/* theme toggler */}
-            
+
             <ThemeToggler />
             {/* mobile nav */}
             <div className="lg:hidden xl:hidden">
