e24e66bd255b0ac8e71e1ba7336ae68f1488dad2
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2024-09-22 03:34:02 -0600

    sleeping time

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,10 +2,35 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { Download, Send } from "lucide-react";
 
-import { RiBriefcase4Fill } from "react-icons/ri";
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+
+//components
+import DevImg from "./DevImg";
+import Badge from "./Badge";
+import Socials from "./Socials";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div>
+          {/* text */}
+          <div>text</div>
+          {/* image */}
+          <div>image</div>
+        </div>
+        {/* icon */}
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary "/>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
