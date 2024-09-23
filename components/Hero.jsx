b2f0e5b38e57eaa8f9402dc4d13e70b0a9f9b559
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2024-09-23 19:01:45 +0000

    epl library update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,17 +16,26 @@ import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <section>
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
-        <div>
+        <div className="flex justify-between gap-x-8">
           {/* text */}
-          <div>text</div>
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left ">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Full Stack Developer</div>
+            <h1 className="h1 mb-4">Hello, my name is Nda Kunene</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Brief description with insights into myself, my vocational
+              journey, and what I engage in professionally.
+            </p>
+            {/* button */}
+            <div>Buttons</div>
+          </div>
           {/* image */}
-          <div>image</div>
+          <div className="hidden xl:flex relative">image</div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
-          <RiArrowDownSLine className="text-3xl text-primary "/>
+          <RiArrowDownSLine className="text-3xl text-primary " />
         </div>
       </div>
     </section>
